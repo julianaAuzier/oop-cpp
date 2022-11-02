d7b1e7a081d1adf419d85727c9716d4070993ebb
Juliana Auzier Seixas Feio <juliana.auzier.s@gmail.com>
2022-11-02 18:36:45 -0300

    Explicação do código

--- areaVolume/Circulo.cpp
+++ areaVolume/Circulo.cpp
@@ -1,37 +1,37 @@
 #include "Circulo.h" //no .h foi declarada a classe e os seus elementos (go check!)
 
-// HeranÁa Simples
+// Heran√ßa Simples
 
-Circulo::Circulo( ): Ponto( ) //construtor da classe recebe 3 double (x,y,raio) como parametros sem necessidade de alguma verificaÁ„o.
-							 // Como cÌrculo herda de ponto, o construtor de Ponto deve ser "lembrado" aqui
+Circulo::Circulo( ): Ponto( ) //construtor da classe recebe 3 double (x,y,raio) como parametros sem necessidade de alguma verifica√ß√£o.
+							 // Como c√≠rculo herda de ponto, o construtor de Ponto recebe os parametros aqui.
 {
-	this->raio = 0; //se n„o È dado nenhum par‚metro, o raio È 0 e na classe Ponto, x e y tambÈm ser„o 0 (go check!)
+	this->raio = 0; //se n√£o √© dado nenhum par√¢metro, o raio √© 0 e na classe Ponto, x e y tamb√©m ser√£o 0 (go check!)
 }
 
-Circulo::Circulo( double xout, double yout, double raio ):Ponto( xout, yout ) //outro construtor da classe, possibilita a verificaÁ„o dos parametros
+Circulo::Circulo( double xout, double yout, double raio ):Ponto( xout, yout ) //outro construtor da classe, possibilita a verifica√ß√£o dos parametros
 {
 	if( raio >= 0.0 )
-	// this->raio (atributo da classe) = raio (par‚metro)
+	// this->raio (atributo da classe) = raio (par√¢metro)
 		this->raio = raio;
 	else
-		this->raio = 1.0; // aqui n„o È aceito raio negativo (atÈ porque n„o faz sentido)
+		this->raio = 1.0; // aqui n√£o √© aceito raio negativo (at√© porque n√£o faz sentido)
 }
 
-Circulo::Circulo( const Circulo &c_out ): Ponto( static_cast< Ponto >( c_out ) ) //construtor de cÛpia da classe, como herda de Ponto, o construtor de cÛdia de ponto È "lembrado".
-																				// Aqui È passado para o contrutor de copia de Ponto a "parte Ponto" do obj Circulo (ou seja,somente x e y)
+Circulo::Circulo( const Circulo &c_out ): Ponto( static_cast< Ponto >( c_out ) ) //construtor de c√≥pia da classe, como herda de Ponto, o construtor de c√≥dia de ponto recebe o objeto ponto aqui.
+																				// Aqui √© passado para o contrutor de copia de Ponto a "parte Ponto" do obj Circulo (ou seja,somente x e y)
 {
-	this->raio = c_out.raio; // o raio È pego separado porque a "parte Ponto" do par‚metro foi mandada para o novo objPonto.
-							// Aqui È add o raio neste objeto e assim ele tambÈm È Circulo
+	this->raio = c_out.raio; // o raio √© pego separado porque a "parte Ponto" do par√¢metro foi mandada para o novo objPonto.
+							// Aqui √© add o raio neste objeto e assim ele tamb√©m √© Circulo
 }
 
 Circulo::~Circulo(){ //destrutor
 }
 
-const Circulo &Circulo::operator= (const Circulo &cirOut ) //sobreposiÁ„o de = (atribuiÁ„o) pra passar os valores de um objeto Circulo pra outro
+const Circulo &Circulo::operator= (const Circulo &cirOut ) //sobreposi√ß√£o de = (atribui√ß√£o) pra passar os valores de um objeto Circulo pra outro
 {
-	*static_cast< Ponto * >( this ) = static_cast< Ponto >( cirOut ); // a "parte Ponto" do objeto ‡ esquerda de = recebe a "parte Ponto" do objeto ‡ direita de =
+	*static_cast< Ponto * >( this ) = static_cast< Ponto >( cirOut ); // a "parte Ponto" do objeto √† esquerda de = recebe a "parte Ponto" do objeto √† direita de =
 	
-	this->raio = cirOut.raio; // o raio È atribuÌdo separadamente
+	this->raio = cirOut.raio; // o raio √© atribu√≠do separadamente
 	
 	return *this; 
 }
@@ -40,7 +40,7 @@ const Circulo &Circulo::operator= (const Circulo &cirOut ) //sobreposiÁ„o de = (
 
 ostream &operator<<( ostream &out, const Circulo &c ) //sobreposicao de <<. Quando o c++ ver a estrutura: objeto da classe ostream (cout) << objeto de Circulo..
 {
-	out << 	"Centro: " << static_cast< Ponto >( c ) << '\t'; // ..ele vai mostrar o centro do cÌculo (a parte Ponto do objeto)
+	out << 	"Centro: " << static_cast< Ponto >( c ) << '\t'; // ..ele vai mostrar o centro do c√≠culo (a parte Ponto do objeto)
 	out << "Raio: " << c.raio;								// e o raio
 		
 	return out;
