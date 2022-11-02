d0798e76c5b14fd74f5cf38b59f6e51f11b7a9f8
Juliana Auzier Seixas Feio <juliana.auzier.s@gmail.com>
2022-11-02 18:50:19 -0300

    Explicação do código

--- areaVolume/Cilindro.cpp
+++ areaVolume/Cilindro.cpp
@@ -1,25 +1,25 @@
 #include "Cilindro.h" //no .h foi declarada a classe e os seus elementos (go check!)
 
-// Herança Multinível
+// HeranÃ§a MultinÃ­vel
 
-Cilindro::Cilindro( ): Circulo(  ) // construtor da classe, como herda de Circulo, então o construtor de Circulo deve ser "lembrado" aqui
+Cilindro::Cilindro( ): Circulo(  ) // construtor da classe, como herda de Circulo, entÃ£o o construtor de Circulo recebe os valores como parametro aqui
 {
 	this->altura = 0;
 }
 
 
-Cilindro::Cilindro( double x, double y, double raio, double altura ) // construtor quando recebe parâmetros que podem passar por verificação
+Cilindro::Cilindro( double x, double y, double raio, double altura ) // construtor quando recebe parÃ¢metros que podem passar por verificaÃ§Ã£o
 :Circulo( x, y, raio ) //os parametros x, y e raio
 {
-	if( altura > 0 ) // a altura deverá ser maior que 0 pra que seja um cilindro, caso contrário será só mais um círculo!
-		this->altura = altura; // a (this) altura (declarada no .h // go check! //) recebe o valor da altura (parâmetro) de forma
-		//						separada porque os outros parâmetros são passados para os atributos da classe Cilindro, serão tratados lá
+	if( altura > 0 ) // a altura deverÃ¡ ser maior que 0 pra que seja um cilindro, caso contrÃ¡rio serÃ¡ sÃ³ mais um cÃ­rculo!
+		this->altura = altura; // o atributo (this) altura (declarada no .h // go check! //) recebe o valor da altura (parÃ¢metro) de forma
+		//separada porque os outros parÃ¢metros sÃ£o passados para os atributos da classe Cilindro, serÃ£o tratados lÃ¡
 	else
-		this->altura = 1; // se a altura dada for <1, o valor será 1
+		this->altura = 1; // se a altura dada for <1, o valor serÃ¡ 1
 }
 
-Cilindro::Cilindro( const Cilindro & circOut ):Circulo( static_cast< Circulo >( circOut ) ) // construtor de cópia: recebe um obj da propria classe e
-//																							   passa a "parte Circulo" desse obj para a classe Circulo
+Cilindro::Cilindro( const Cilindro & circOut ):Circulo( static_cast< Circulo >( circOut ) ) // construtor de cÃ³pia: recebe um obj da propria classe e
+//passa a "parte Circulo" desse obj para a classe Circulo
 {
 	this->altura = circOut.altura; // pega a altura (atributo a+ que tem em Cilindro) separadamente
 }
@@ -30,10 +30,10 @@ Cilindro::~Cilindro() //destrutor
 {
 }
 
-const Cilindro &Cilindro::operator=( const Cilindro &cilOut ) // sobreposição do operador = (atribuição) para um obj da classe pegue
-//																 os mesmos valores de outro obj da propria classe
+const Cilindro &Cilindro::operator=( const Cilindro &cilOut ) // sobreposiÃ§Ã£o do operador = (atribuiÃ§Ã£o) para um obj da classe pegue
+//os mesmos valores de outro obj da propria classe
 {
-	*static_cast< Circulo * >( this ) = static_cast< Circulo >( cilOut ); // a "parte Circulo" do objeto à esquerda de = recebe a "parte Circulo" do objeto à direita de =
+	*static_cast< Circulo * >( this ) = static_cast< Circulo >( cilOut ); // a "parte Circulo" do objeto Ã  esquerda de = recebe a "parte Circulo" do objeto Ã  direita de =
 	
 	this->altura = cilOut.altura;
 	
@@ -42,7 +42,7 @@ const Cilindro &Cilindro::operator=( const Cilindro &cilOut ) // sobreposição do
 
 ostream &operator<<( ostream &out, const Cilindro &cil ) //sobreposicao de <<. Quando o c++ ver a estrutura: objeto da classe ostream (cout) << objeto de Cilindro..
 {
-	out << 	"Base: " << static_cast< Circulo >( cil ) << '\n'; // ..ele vai mostrar a base do cilindro, que é um circulo (a parte Circulo do objeto)
+	out << 	"Base: " << static_cast< Circulo >( cil ) << '\n'; // ..ele vai mostrar a base do cilindro, que Ã© um circulo (a parte Circulo do objeto)
 	out << "A altura eh " << cil.altura;
 		
 	return out;
